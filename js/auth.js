d910e0f0addd377a960dc0a402e9cc125fbde3f7
wissam00125 <wissamh1091@gmail.com>
2025-04-20 00:36:16 +0300

    Implement user registration and login with email validation

--- js/auth.js
+++ js/auth.js
@@ -10,11 +10,79 @@ document.addEventListener("DOMContentLoaded", () => {
     registerContainer.classList.toggle("hidden");
     loginContainer.classList.toggle("hidden");
 
-    console.log("showlogin clicked");
+    // console.log("showlogin clicked");
   });
   showRegisterForm.addEventListener("click", (e) => {
     e.preventDefault();
     loginContainer.classList.toggle("hidden");
     registerContainer.classList.toggle("hidden");
   });
+});
+
+const registerBtn = document.querySelector(".register-btn");
+registerBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const registerName = document.querySelector(".register-name");
+  const registerEmail = document.querySelector(".register-email");
+  const registerPassword = document.querySelector(".register-password");
+
+  let users = JSON.parse(localStorage.getItem("users")) || [];
+
+  const user = users.find(
+    (user) => user.email.toLowerCase() === registerEmail.value.toLowerCase()
+  );
+
+  if (user) {
+    const registerEmail = document.querySelector(".register-email");
+    const oldWarning = document.getElementById("email-warning");
+    if (oldWarning) {
+      oldWarning.remove();
+    }
+    const span = document.createElement("span");
+    span.id = "email-warning";
+    span.textContent = "This email exist! try another one";
+    span.style.color = "red";
+    span.style.fontSize = "12px";
+    registerEmail.parentNode.insertBefore(span, registerEmail.nextSibling);
+    return;
+  }
+
+  // console.log(registerName.value, registerEmail.value, registerPassword.value);
+  const userId = Date.now().toString();
+
+  const newUser = {
+    id: userId,
+    name: registerName.value.trim(),
+    email: registerEmail.value.trim(),
+    password: registerPassword.value.trim(),
+  };
+
+  users.push(newUser);
+  localStorage.setItem("users", JSON.stringify(users));
+  localStorage.setItem("currentUser", JSON.stringify(newUser));
+
+  window.location.href = "home.html";
+});
+
+const loginBtn = document.querySelector(".login-btn");
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const loginEmail = document.querySelector(".login-email");
+  const loginPassword = document.querySelector(".login-password");
+
+  let users = JSON.parse(localStorage.getItem("users")) || [];
+  const user = users.find(
+    (user) =>
+      user.email.toLowerCase() === loginEmail.value.toLowerCase() &&
+      user.password === loginPassword.value
+  );
+
+  if (user) {
+    localStorage.setItem("currentUser", JSON.stringify(user));
+    if (user.email.toLowerCase() === "admin@gmail.com") {
+      window.location.href = "dashboard.html";
+    } else {
+      window.location.href = "home.html";
+    }
+  }
 });
