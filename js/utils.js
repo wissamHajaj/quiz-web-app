c3a1ebdb75513dcc6fba685d33bd497564d11fb2
wissam00125 <wissamh1091@gmail.com>
2025-04-21 19:18:29 +0300

    Add utils.js with common functions: logout, password and email validation

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,17 @@
 function logout() {
-    localStorage.removeItem("currentUser");
-    window.location.href = "index.html";
+  localStorage.removeItem("currentUser");
+  window.location.href = "index.html";
+}
+
+function validatePassword(password) {
+  return password.length >= 8;
+}
+
+const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+function validateEmail(email) {
+  if (!emailRegex.test(email)) {
+    return false;
   }
+  return true;
+}
